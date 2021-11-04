9c4de44c61f71c498673985d504a2049ad3f942b
Jakub Lyczko <jakub.lyczko1@gmail.com>
2021-11-04 12:58:34 +0100

    Replace old vuepress with new one working on vite

--- docs/.vuepress/plugins/sidebar/index.js
+++
@@ -1,7 +0,0 @@
-const { path } = require("@vuepress/shared-utils");
-
-module.exports = (options = {}, ctx) => {
-  return {
-    enhanceAppFiles: [path.resolve(__dirname, "enhanceAppFile.js")],
-  };
-};
---
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -0,0 +1,22 @@
+<template>
+  <Header />
+  <main><Content /></main>
+  <Footer />
+</template>
+
+<script>
+import Header from './Header/Header.vue';
+import Footer from './Footer/Footer.vue';
+export default {
+  components: {
+    Header,
+    Footer,
+  },
+};
+</script>
+
+<style>
+main {
+  height: 1000px;
+}
+</style>
