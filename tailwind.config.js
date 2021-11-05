42288b30420127f80f0d8574ef7b65e613ff32b7
Jakub Lyczko <jakub.lyczko1@gmail.com>
2021-11-05 13:14:53 +0100

    Remove needless configs and options

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,10 @@
 module.exports = {
   plugins: [require('tailwindcss')],
   purge: {
-    enabled: process.env.NODE_ENV === 'production',
     content: [
       './docs/.vuepress/**/*.js',
       './docs/.vuepress/**/*.vue',
       './docs/.vuepress/**/*.ts',
     ],
-    options: {
-      safelist: ['html', 'body'],
-    },
   },
 };
